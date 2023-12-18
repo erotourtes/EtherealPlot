6bf19b39603d77ad40cadfd82063fb9d9ed3c673
erotourtes <erotourtes@gmail.com>
2023-12-18 11:33:35 +0200

    MainActivity uses EtherealPlotApp

--- app/src/main/java/com/github/erotourtes/MainActivity.kt
+++ app/src/main/java/com/github/erotourtes/MainActivity.kt
@@ -3,20 +3,11 @@ package com.github.erotourtes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import com.github.erotourtes.screen.main.Home
-import com.github.erotourtes.ui.theme.AppTheme
+import com.github.erotourtes.ui.EtherealPlotApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            AppTheme {
-                Surface(color = MaterialTheme.colorScheme.background) {
-                    Home()
-                }
-            }
-        }
+        setContent { EtherealPlotApp() }
     }
 }
