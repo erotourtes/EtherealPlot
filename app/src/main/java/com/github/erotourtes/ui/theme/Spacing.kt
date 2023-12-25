e382b710f55882117b15bb28387fb8d550984297
erotourtes <erotourtes@gmail.com>
2023-12-25 16:04:57 +0200

    Add: immutable to spacing data class

--- app/src/main/java/com/github/erotourtes/ui/theme/Spacing.kt
+++ app/src/main/java/com/github/erotourtes/ui/theme/Spacing.kt
@@ -2,11 +2,13 @@ package com.github.erotourtes.ui.theme
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
+@Immutable
 data class Spacing(
     val default: Dp = 2.dp,
     val small: Dp = 4.dp,
